6f49c9dd157648c7426f2e6ecc682240ca22d8c8
Wiecio <wiecio1@gmail.com>
2022-05-01 21:13:11 +0200

    Initial commit

---
+++ ASP.NET tutorial/ASP.NET tutorial/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ASP.NET_tutorial.Models;
+
+namespace ASP.NET_tutorial.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<ASP.NET_tutorial.Models.Joke> Joke { get; set; }
+}
